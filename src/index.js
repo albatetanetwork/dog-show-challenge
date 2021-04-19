c942865760b113f2a5f2bd48e3924d73304c6264
Alba <albatetanetwork@gmail.com>
2021-04-18 23:05:27 -0400

    Partially Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    window.onload = (event) => {
+        fetch('http://localhost:3000/dogs') 
+            .then(r => r.json())
+            .then(data => console.log(data));
+      };
+
 })
